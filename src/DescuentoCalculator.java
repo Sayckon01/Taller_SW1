7637db762e7a0ff197c9de9e897dcf3eee10e10d
Edwin Alexander Gonzalez Blandon <136096648+Sayckon01@users.noreply.github.com>
2023-09-01 00:26:27 -0500

    Update DescuentoCalculator.java
    Actualización de nombre de variable subtotal -> valorDescuento

--- src/DescuentoCalculator.java
+++ src/DescuentoCalculator.java
@@ -2,14 +2,14 @@ import javax.swing.JOptionPane;
 public class DescuentoCalculator {
 
 	public static void main(String[] args) {
-		double precio,descuento,subtotal,total;
+		double precio,descuento,valorDescuento,total;
 		JOptionPane.showMessageDialog(null, "      Calculadora de descuentos\npresione aceptar para continuar...");
 		precio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio del producto"));
 		descuento = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el % de descuento del producto"));
 		descuento/=100;
-		subtotal = precio*descuento;
-		total=precio-subtotal;
-		JOptionPane.showMessageDialog(null, "Precio: $"+precio+"\nDescuento "+(descuento*100)+"%: $"+subtotal+"\nTotal a pagar: $"+total);
+		valorDescuento = precio*descuento;
+		total=precio-valorDescuento;
+		JOptionPane.showMessageDialog(null, "Precio: $"+precio+"\nDescuento "+(descuento*100)+"%: $"+valorDescuento+"\nTotal a pagar: $"+total);
 
 	}
 
